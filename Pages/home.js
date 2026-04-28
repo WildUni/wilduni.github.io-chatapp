77b3ec34896a410e45f9daa4c19f6edbefdf818e
wilduni <wilduni@mit.edu>
2026-04-27 21:01:59 -0400

    working on new chat

--- Pages/home.js
+++ Pages/home.js
@@ -1,4 +1,4 @@
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import loadChatList from "../Components/chatList.js";
 import loadChatTree from "../Components/chatTree.js";
@@ -8,20 +8,39 @@ import loadChatFlow from "../Components/chatFlow.js";
 
 import { storeToRefs } from "pinia"
 import { useActiveChatStore } from "../stores/activeChat.js";
+import { useNewChatStore } from "../stores/newChat.js";
+
+import {
+  GraffitiPlugin,
+  useGraffiti,
+  useGraffitiSession,
+  useGraffitiDiscover,
+} from "@graffiti-garden/wrapper-vue";
+
 
 function setup() {
   const route = useRoute()
   const router = useRouter()
+  const session = useGraffitiSession();
+  
 
   const chats = ref([1, 2])
 
-  const activeChat = useActiveChatStore()
+  const activeChat = useActiveChatStore();
   const { activeChatId } = storeToRefs(activeChat)
 
+  const newChat = useNewChatStore();
+
   function updateChatUrl(chatId) {
+    activeChat.setActiveChat(chatId);
     router.push({ name: "chat", params: { chatId } })
+    console.log(activeChat.activeChatId)
   }
 
+  function clearActive(){
+    activeChat.setActiveChat(null);
+    console.log(activeChat.activeChatId)
+  }
   watch(
     () => route.params.chatId,
     (chatId) => {
@@ -30,10 +49,21 @@ function setup() {
     { immediate: true }
   )
 
+  function printActive(){
+    console.log(session.value.actor)
+  }
+  console.log(activeChat)
+  console.log(newChat.setNewChatName)
+
   return {
     chats,
     updateChatUrl,
-    activeChatId
+    activeChatId,
+    clearActive,
+    printActive,
+    createNewChat: newChat.createNewChat,
+    newChatName: newChat.newChatName,
+    setNewChatName: newChat.setNewChatName,
   }
 }
 
---
+++ stores/newChat.js
@@ -0,0 +1,76 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import {
+  useGraffiti,
+  useGraffitiSession,
+} from "@graffiti-garden/wrapper-vue";
+
+import { useActiveChatStore } from "./activeChat.js";
+
+export const useNewChatStore = defineStore("newChat", () => {
+    const graffiti = useGraffiti();
+    const session = useGraffitiSession();
+    const activeChat = useActiveChatStore();
+
+
+    const newChatName = ref('');
+    
+    function setNewChatName(name) {
+        newChatName.value = name;
+    }
+    
+    async function createNewChat(parent = null){
+        if(!newChatName.value) return
+        
+        const chatId = crypto.randomUUID();
+        graffiti.post(
+            {
+                value:{
+                    action: 'Membership',
+                    value: 'Join',
+                    id: chatId, 
+                    name: newChatName.value,
+                    published: Date.now(),
+                },
+                channels: [`user:${session.value.actor}:Membership`],
+                allowed: []
+            },
+            session.value      
+        )
+        graffiti.post(
+            {
+                value:{
+                    action: 'Create',
+                    id: chatId, 
+                    name: newChatName.value,
+                    published: Date.now(),
+                },
+                channels: [`Chat:${chatId}:Activity`],
+            },
+            session.value      
+        )
+        graffiti.post(
+            {
+                value:{
+                    action: 'Membership',
+                    value: 'Join',
+                    user: session.value.actor,
+                    published: Date.now(),
+                    parent
+                },
+                channels: [`Chat:${chatId}:Membership`],
+            },
+            session.value      
+        )
+        activeChat.setActiveChat(chatId);
+        newChatName.value = "";
+    }
+
+    return {
+        newChatName,
+        createNewChat,
+        setNewChatName
+    }
+});
+
+
